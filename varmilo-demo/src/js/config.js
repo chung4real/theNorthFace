087b6ab12049684411bb02cab13be97b78c0b4ce
chung <xiaozhou.cd@gmail.com>
2020-10-19 09:06:30 +0800

    feat:使用swiper完成index.html轮播图功能

--- varmilo-demo/src/js/config.js
+++ varmilo-demo/src/js/config.js
@@ -3,10 +3,11 @@ require.config({
   paths: {
     // 给每一个模块配置名字以及路径，路径不能写后缀名.js
     jquery: '/libs/jquery-3.5.1.min',
-    bootstrap: '/bootstrap/css/bootstrap.min',
     header: '/js/modules/header',
     footer: '/js/modules/footer',
-    template: '/libs/art-template/template-web'
+    template: '/libs/art-template/template-web',
+    swiper: '/libs/swiper/js/swiper.min',
+    bootstrap: '/bootstrap/css/bootstrap.min',
   },
   // 垫片
   shim: {
--- varmilo-demo/src/js/index.js
+++ varmilo-demo/src/js/index.js
@@ -1,10 +1,48 @@
 require(['./config'], () => {
-  require(['header', 'footer'], () => {
+  require(['swiper', 'header', 'footer'], (swiper) => {
     class Index {
       constructor() {
-        
+        // this.moveTop()
+
       }
+      // 轮播图效果
+      // moveTop() {
+
+      // } 
     }
     new Index()
+    var mySwiper = new swiper('.swiper-container', {
+      loop: true, // 循环模式选项
+      speed: 1500,
+      autoplay: {
+        delay: '2000',// 轮播间隔时间  delay >= speed 否则效果无法运行
+        disableOnInteraction: false
+      },
+      effect: 'fade',
+      // effect: 'cube',
+      // effect:'coverflow',
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type:'progressbar'
+      },
+      // 如果需要前进后退按钮
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+    })
+    mySwiper.el.onmouseover = function () {
+      mySwiper.navigation.$nextEl.removeClass('hide');
+      mySwiper.navigation.$prevEl.removeClass('hide');
+      // 用户移入轮播图停止播放
+      mySwiper.autoplay.stop()
+    }
+    mySwiper.el.onmouseout = function () {
+      mySwiper.navigation.$nextEl.addClass('hide');
+      mySwiper.navigation.$prevEl.addClass('hide');
+      // 用户移除轮播图继续播放
+      mySwiper.autoplay.start()
+    }
   })
 })
